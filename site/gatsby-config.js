37c8d8a231c0cd1ed0b254d664d6aa67584d2ebd
psyauto <wildshot01@gmail.com>
2021-04-23 09:11:27 +0300

    modify WP backend url

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -1,13 +1,16 @@
 require('dotenv').config();
 
 module.exports = {
+	siteMetadata: {
+	   siteUrl: "https://quizzical-blackwell.185-4-134-158.plesk.page",
+	  },
 	plugins: [
 		// Tell gatsby which theme you will be using and other configs.
 		{
 		resolve: "gatsby-woocommerce-theme",
 		options: {
-			wordPressUrl: `${ process.env.WORDPRESS_SITE_URL }`,
-			gatsbySiteUrl: `${ process.env.GATSBY_SITE_URL }`,
+			wordPressUrl: "https://quizzical-blackwell.185-4-134-158.plesk.page",
+			gatsbySiteUrl: "https://quizzical-blackwell.185-4-134-158.plesk.page",
 			googleTagManagerId: `${ process.env.GOOGLE_TAGMANAGER_ID }`,
 			fbAppId: `${ process.env.FB_APP_ID }`
 		}
